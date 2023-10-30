b3f24b7ce4af8b89f24b8640fbed93c7e2d706f8
vjose2807 <vjose2807@gmail.com>
2023-10-29 22:06:50 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
-function App() {
+const MIN_LENGTH = 1;
+const MAX_LENGTH = 60;
+
+const App = () => {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timerLabel, setTimerLabel] = useState("Session");
+  const [timeLeft, setTimeLeft] = useState(sessionLength * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const beepSound = useRef(null);
+
+  const reset = () => {
+    beepSound.current.pause();
+    beepSound.current.currentTime = 0;
+    setIsRunning(false);
+    setTimerLabel("Session");
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimeLeft(25 * 60);
+  };
+
+  const handleLengthChange = (newLength, setLength) => {
+    if (!isRunning && newLength >= MIN_LENGTH && newLength <= MAX_LENGTH) {
+      setLength(newLength);
+      setTimeLeft(newLength * 60);
+    }
+  };
+
+  const toggleTimer = () => {
+    setIsRunning(!isRunning);
+  };
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning && timeLeft > 0) {
+      interval = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      beepSound.current.play();
+
+      if (timerLabel === "Session") {
+        setTimerLabel("Break");
+        setTimeLeft(breakLength * 60);
+      } else if (timerLabel === "Break") {
+        setTimerLabel("Session");
+        setTimeLeft(sessionLength * 60);
+      }
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning, timeLeft, breakLength, sessionLength, timerLabel]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="window glass-effect">
+      <div className="title">Pomodoro Timer</div>
+      <div id="break-label" className="label">
+        Break Length
+      </div>
+      <div className="buttons">
+        <button
+          id="break-decrement"
+          className="button"
+          onClick={() => handleLengthChange(breakLength - 1, setBreakLength)}
         >
-          Learn React
-        </a>
-      </header>
+          Decrease
+        </button>
+        <div id="break-length" className="value">
+          {breakLength}
+        </div>
+        <button
+          id="break-increment"
+          className="button"
+          onClick={() => handleLengthChange(breakLength + 1, setBreakLength)}
+        >
+          Increase
+        </button>
+      </div>
+
+      <div id="session-label" className="label">
+        Session Length
+      </div>
+      <div className="buttons">
+        <button
+          id="session-decrement"
+          className="button"
+          onClick={() =>
+            handleLengthChange(sessionLength - 1, setSessionLength)
+          }
+        >
+          Decrease
+        </button>
+        <div id="session-length" className="value">
+          {sessionLength}
+        </div>
+        <button
+          id="session-increment"
+          className="button"
+          onClick={() =>
+            handleLengthChange(sessionLength + 1, setSessionLength)
+          }
+        >
+          Increase
+        </button>
+      </div>
+
+      <div id="timer-label" className="label">
+        {timerLabel}
+      </div>
+      <div id="time-left" className="clock">
+        {formatTime(timeLeft)}
+      </div>
+
+      <div className="buttons no-border">
+        <button id="start_stop" className="button" onClick={toggleTimer}>
+          {isRunning ? "Pause" : "Start"}
+        </button>
+        <button id="reset" className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+
+      <audio
+        id="beep"
+        src="https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg"
+        ref={beepSound}
+      ></audio>
     </div>
   );
-}
+};
 
 export default App;
